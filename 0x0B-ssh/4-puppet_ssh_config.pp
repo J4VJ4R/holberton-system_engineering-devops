a578c1aa9dbed932c8946a0322b174191b6c2b0c
Javier <ingenierodesoftware73@gmail.com>
2020-08-04 16:32:07 +0000

    fix all code

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
-#script to conect without password
+# Puppet conf SSH client_configuration
 file_line { 'Turn off passwd auth':
-    ensure => 'present',
-    path   => '/etc/ssh/ssh_config',
-    line   => 'PasswordAuthentication no',
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no'
 }
 
 file_line { 'Declare identity file':
-    ensure => 'present',
-    path   => 'etc/ssh/ssh_config',
-    line   => 'IdentityFile ~/.ssh/holberton',
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton'
 }
