82ad1665a5ee867800bb843f1b9d71d873daa969
Javier <ingenierodesoftware73@gmail.com>
2020-09-18 02:06:39 +0000

    create an acount

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -6,4 +6,4 @@ from requests import get
 def count_words(subreddit, word_list):
     """ recursive function that queries the Reddit API, parses the title of
     all hot articles, and prints a sorted count of given keywords. """
-    headers = {"User-Agent": "javierandresgp"}
+    headers = {"User-Agent": "javjarfullstack"}
